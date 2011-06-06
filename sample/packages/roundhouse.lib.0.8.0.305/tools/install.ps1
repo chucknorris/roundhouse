f4a0200d1821b3b63b94dd2d582d512968bf04e0
Rob Reynolds <ferventcoder@gmail.com>
2011-06-05 23:28:19 -0500

    updating the sample application and getting it working nicely with the new refreshdatabase.

---
+++ sample/packages/roundhouse.lib.0.8.0.305/tools/install.ps1
@@ -0,0 +1,5 @@
+param($installPath, $toolsPath, $package, $project)
+
+Write-Host 'RoundhousE - Professional Database Versioning and Change Management'
+Write-Host 'This is the library (dll) version of roundhouse. Please have a look at this gist for how to use it: https://gist.github.com/977990'
+Write-Host 'RoundhousE also comes in other flavors. Please take a look at http://nuget.org/List/Search?searchTerm=tag%3A%20roundhouse'
