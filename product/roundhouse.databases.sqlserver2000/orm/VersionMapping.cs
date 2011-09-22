c00d405d2eb9461109e2c9d173aee261efe34c03
Rob Reynolds <ferventcoder@gmail.com>
2011-09-22 05:07:17 -0500

    fix for issue 18 - sql2000 is only creating scrips run errors table.

--- product/roundhouse.databases.sqlserver2000/orm/VersionMapping.cs
+++ product/roundhouse.databases.sqlserver2000/orm/VersionMapping.cs
@@ -10,7 +10,7 @@
         public VersionMapping()
         {
             //HibernateMapping.Schema(ApplicationParameters.CurrentMappings.roundhouse_schema_name);
-            Table(ApplicationParameters.CurrentMappings.scripts_run_errors_table_name);
+            Table(ApplicationParameters.CurrentMappings.version_table_name);
             Not.LazyLoad();
             HibernateMapping.DefaultAccess.Property();
             HibernateMapping.DefaultCascade.SaveUpdate();
