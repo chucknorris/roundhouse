a9f7619e1cec28bbc228e5f418203099c0b8a180
Rob Reynolds <ferventcoder@gmail.com>
2011-11-14 06:47:50 -0600

    updating to remove mono.security

--- product/roundhouse.databases.postgresql/PostgreAdoNetProviderResolver.cs
+++ product/roundhouse.databases.postgresql/PostgreAdoNetProviderResolver.cs
@@ -1,17 +1,17 @@
 ﻿namespace roundhouse.databases.postgresql
 {
-	using System;
-	using System.Configuration;
-	using System.Data;
-	using System.Linq;
-	using System.Reflection;
-	using roundhouse.infrastructure;
+    using System;
+    using System.Configuration;
+    using System.Data;
+    using System.Linq;
+    using System.Reflection;
+    using infrastructure;
 
-	public class PostgreAdoNetProviderResolver
-	{
-		private readonly bool is_merged;
+    public class PostgreAdoNetProviderResolver
+    {
+        private readonly bool is_merged;
 
-		public PostgreAdoNetProviderResolver()
+        public PostgreAdoNetProviderResolver()
         {
             is_merged = Assembly.GetExecutingAssembly().GetName().Name == ApplicationParameters.get_merged_assembly_name();
         }
@@ -22,7 +22,7 @@
 
             var my_sql_client_row = dataSet.Tables[0]
                 .AsEnumerable()
-				.Where(x => x.Field<string>(2) == "Npgsql")
+                .Where(x => x.Field<string>(2) == "Npgsql")
                 .SingleOrDefault();
 
             if (my_sql_client_row != null)
@@ -30,12 +30,12 @@
                 dataSet.Tables[0].Rows.Remove(my_sql_client_row);
             }
 
-			var factory_type = is_merged ? "Npgsql.NpgsqlFactory, " + ApplicationParameters.get_merged_assembly_name() : "Npgsql.NpgsqlFactory, Npgsql";
+            var factory_type = is_merged ? "Npgsql.NpgsqlFactory, " + ApplicationParameters.get_merged_assembly_name() : "Npgsql.NpgsqlFactory, Npgsql";
 
             dataSet.Tables[0].Rows.Add(
                 "PostgreSQL Data Provider",
-				".Net Framework Data Provider for PostgreSQL",
-				"Npgsql",
+                ".Net Framework Data Provider for PostgreSQL",
+                "Npgsql",
                 factory_type);
         }
 
@@ -46,12 +46,12 @@
 
         private Assembly load_mysql_data_assembly(string assembly_name)
         {
-			if (is_merged && assembly_name == "Npgsql")
+            if (is_merged && assembly_name == "Npgsql")
             {
                 return Assembly.GetExecutingAssembly();
             }
 
             return null;
-        } 
-	}
+        }
+    }
 }
