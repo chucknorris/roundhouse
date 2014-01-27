d441bf523805a8b0d242358f87ae91f2f80c2f47
Alexander Harris <alexandermlharris@gmail.com>
2014-01-27 21:03:36 +0000

    Type

--- product/roundhouse/runners/RoundhouseMigrationRunner.cs
+++ product/roundhouse/runners/RoundhouseMigrationRunner.cs
@@ -23,7 +23,7 @@ namespace roundhouse.runners
         private readonly VersionResolver version_resolver;
         public bool silent { get; set; }
         public bool dropping_the_database { get; set; }
-        private readonly bool dont_create_the_database;b
+        private readonly bool dont_create_the_database;
         private bool run_in_a_transaction;
         private readonly bool use_simple_recovery;
         private readonly ConfigurationPropertyHolder configuration;
