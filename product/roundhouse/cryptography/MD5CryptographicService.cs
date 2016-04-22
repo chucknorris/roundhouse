22b7b27c2f3edfc73e49c39068843aa1e2e7f6d3
us345848 <rfbain1@mmm.com>
2016-04-22 08:47:08 -0500

    Normalizing line endings prior to computing hash.

--- product/roundhouse/cryptography/MD5CryptographicService.cs
+++ product/roundhouse/cryptography/MD5CryptographicService.cs
@@ -15,7 +15,14 @@ namespace roundhouse.cryptography
 
         public string hash(string clear_text_of_what_to_hash)
         {
-            byte[] clear_text_bytes = Encoding.UTF8.GetBytes(clear_text_of_what_to_hash);
+            const string line_ending_windows = "\r\n";
+            const string line_ending_unix = "\n";
+            const string line_ending_mac = "\r";
+            string normalized_text_of_what_to_hash = clear_text_of_what_to_hash
+                .Replace(line_ending_windows, line_ending_unix).Replace(line_ending_mac, line_ending_unix);
+
+            byte[] clear_text_bytes = Encoding.UTF8.GetBytes(normalized_text_of_what_to_hash);
+
             byte[] cypher_bytes = crypto_provider.ComputeHash(clear_text_bytes);
             return Convert.ToBase64String(cypher_bytes);
         }
