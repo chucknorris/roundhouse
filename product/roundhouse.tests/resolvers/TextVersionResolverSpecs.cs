c16c0f902e2e533d352b15e0a19e05ae3df3b882
Matthew MacFarland <Matthew MacFarland>
2016-05-19 15:16:17 -0500

    # This is a combination of 4 commits. # The first commit's message is: (GH-240) Fix for bug: When reading the Version file the content is not trimmed.
    Added trim to the TextVersionResolver class where it reads the version
    from a text file. Created supporting unit test.
    
    # This is the 2nd commit message:
    
    Added Trim to TextVersionResolver and a supporting test.
    
    # This is the 3rd commit message:
    
    Added a .DotSettings file with options to match current code base style. Started with Chocolatey file as based and tweaked it from there to match the RoundhousE code.
    
    # This is the 4th commit message:
    
    Changed edits from tab to spaces indentation to match prevailing code style.

---
+++ product/roundhouse.tests/resolvers/TextVersionResolverSpecs.cs
@@ -0,0 +1,57 @@
+﻿namespace roundhouse.tests.resolvers
+{
+    using System;
+    using bdddoc.core;
+    using developwithpassion.bdd.contexts;
+    using developwithpassion.bdd.mbunit;
+    using developwithpassion.bdd.mbunit.standard;
+    using developwithpassion.bdd.mbunit.standard.observations;
+    using roundhouse.infrastructure.filesystem;
+    using roundhouse.resolvers;
+    using Rhino.Mocks;
+
+    public class TextVersionResolverSpecs
+    {
+        public abstract class concern_for_textversion_resolver : observations_for_a_sut_with_a_contract<VersionResolver, TextVersionResolver>
+        {
+            [CLSCompliant(false)]
+            protected static VersionResolver the_resolver;
+        }
+
+        public abstract class concerns_using_a_fake_filesystem : concern_for_textversion_resolver
+        {
+            protected static FileSystemAccess the_filesystem;
+            protected static string the_versionfile;
+            private context c = () =>
+            {
+                the_filesystem = an<FileSystemAccess>();
+                the_versionfile = string.Format(@"{0}.txt", Guid.NewGuid());
+                provide_a_basic_sut_constructor_argument(the_filesystem);
+                provide_a_basic_sut_constructor_argument(the_versionfile);
+            };
+        }
+
+        [Concern(typeof(TextVersionResolver))]
+        public class when_asking_the_resolver_for_the_version_the_version_text_is_trimmed : concerns_using_a_fake_filesystem
+        {
+            private const string untrimmed = " 1.3.837.1342 \r\n";
+            private const string trimmed = "1.3.837.1342";
+            private static string result;
+            private context c =
+                () =>
+                {
+                    the_filesystem.Stub(x => x.file_exists(the_versionfile)).Return(true);
+                    the_filesystem.Stub(x => x.read_file_text(the_versionfile)).Return(untrimmed);
+                    the_filesystem.Stub(x => x.get_full_path(the_versionfile)).Return(the_versionfile);
+                };
+
+            private because b = () => { result = sut.resolve_version(); };
+
+            [Observation]
+            public void untrimmed_version_from_file_is_trimmed_when_resolved()
+            {
+                result.should_be_equal_to(trimmed);
+            }
+        }
+    }
+}
