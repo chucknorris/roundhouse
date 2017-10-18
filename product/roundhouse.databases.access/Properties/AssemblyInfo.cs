7735e90148b5672b25e25db9d8ae15f95cf565e0
Erik A. Brandstadmoen <erik@brandstadmoen.net>
2017-10-18 22:09:55 +0200

    Removed strong-naming. Not used widely anymore elsewhere, and newest version of nuget refs (esp. StructureMap) is not strong-named anymore

--- product/roundhouse.databases.access/Properties/AssemblyInfo.cs
+++ product/roundhouse.databases.access/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d1df5985-48bc-48b8-ba40-8c71920ac918")]
-[assembly: AssemblyKeyFile("..\\..\\RoundhousE.snk")]
+//[assembly: AssemblyKeyFile("..\\..\\RoundhousE.snk")]
--- product/roundhouse.databases.oracle/Properties/AssemblyInfo.cs
+++ product/roundhouse.databases.oracle/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f9da0a8d-edee-4941-86ce-2a138b32286d")]
-[assembly: AssemblyKeyFile("..\\..\\RoundhousE.snk")]
+//[assembly: AssemblyKeyFile("..\\..\\RoundhousE.snk")]
