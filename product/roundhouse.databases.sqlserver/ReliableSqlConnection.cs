9d0bd42cc142bb402773ef3572a04cbf1f5ae567
Erik A. Brandstadmoen <erik@brandstadmoen.net>
2018-04-29 14:11:58 +0200

    Fixed some formatting in ReliableSqlConnection

--- product/roundhouse.databases.sqlserver/ReliableSqlConnection.cs
+++ product/roundhouse.databases.sqlserver/ReliableSqlConnection.cs
@@ -6,23 +6,31 @@ using System.Xml;
 using Polly;
 using Polly.Retry;
 
+/**
+ * Ported to .NET Core from Enterprise Library - Transient Fault Handling Application Block:
+ * Introduction: http://msdn.microsoft.com/entlib
+ * Nuget: https://www.nuget.org/packages/EnterpriseLibrary.TransientFaultHandling/
+ */
+
 namespace roundhouse.databases.sqlserver
 {
-    public sealed class ReliableSqlConnection : IDbConnection, IDisposable, ICloneable
+    public sealed class ReliableSqlConnection : IDbConnection, ICloneable
     {
-        private readonly static TransientErrorDetectionStrategy error_detection_strategy = new TransientErrorDetectionStrategy();
+        private static readonly TransientErrorDetectionStrategy error_detection_strategy = new TransientErrorDetectionStrategy();
         
-        private readonly SqlConnection underlyingConnection;
-        private readonly RetryPolicy connectionRetryPolicy = GetDefaultRetryPolicy();
-
-        private readonly RetryPolicy commandRetryPolicy = GetDefaultRetryPolicy();
-        private readonly RetryPolicy connectionStringFailoverPolicy = GetDefaultRetryPolicy();
-        private string connectionString;
-        private static RetryPolicy defaultConnectionRetryPolicy = GetDefaultRetryPolicy();
-        private static RetryPolicy defaultCommandRetryPolicy = GetDefaultRetryPolicy();
+        private readonly SqlConnection underlying_connection;
+        
+        private static readonly RetryPolicy default_connection_retry_policy = get_default_retry_policy();
+        private static readonly RetryPolicy default_command_retry_policy = get_default_retry_policy();
+        
+        private readonly RetryPolicy connection_retry_policy;
+        private readonly RetryPolicy command_retry_policy;
+        private readonly RetryPolicy connection_string_failover_policy = get_default_retry_policy();
+        
+        private string connection_string;
         
        
-        private static RetryPolicy GetDefaultRetryPolicy()
+        private static RetryPolicy get_default_retry_policy()
         {
             return Policy
                 .Handle<Exception>(ex => error_detection_strategy.is_transient(ex))
@@ -43,9 +51,9 @@ namespace roundhouse.databases.sqlserver
         /// Initializes a new instance of the <see cref="T:roundhouse.databases.sqlserver.ReliableSqlConnection" /> class with the specified connection string. Uses the default
         /// retry policy for connections and commands unless retry settings are provided in the connection string.
         /// </summary>
-        /// <param name="connectionString">The connection string used to open the SQL Database.</param>
-        public ReliableSqlConnection(string connectionString)
-            : this(connectionString, defaultConnectionRetryPolicy)
+        /// <param name="connection_string">The connection string used to open the SQL Database.</param>
+        public ReliableSqlConnection(string connection_string)
+            : this(connection_string, default_connection_retry_policy)
         {
         }
 
@@ -57,7 +65,7 @@ namespace roundhouse.databases.sqlserver
         /// <param name="connectionString">The connection string used to open the SQL Database.</param>
         /// <param name="retryPolicy">The retry policy that defines whether to retry a request if a connection or command fails.</param>
         public ReliableSqlConnection(string connectionString, RetryPolicy retryPolicy)
-            : this(connectionString, retryPolicy, defaultCommandRetryPolicy ?? retryPolicy)
+            : this(connectionString, retryPolicy, default_command_retry_policy ?? retryPolicy)
         {
         }
 
@@ -71,22 +79,23 @@ namespace roundhouse.databases.sqlserver
         /// <param name="commandRetryPolicy">The retry policy that defines whether to retry a request if a command fails to be executed.</param>
         public ReliableSqlConnection(string connectionString, RetryPolicy connectionRetryPolicy, RetryPolicy commandRetryPolicy)
         {
-            this.connectionString = connectionString;
-            underlyingConnection = new SqlConnection(connectionString);
-            this.connectionRetryPolicy = connectionRetryPolicy;
-            this.commandRetryPolicy = commandRetryPolicy;
+            this.connection_string = connectionString;
+            underlying_connection = new SqlConnection(connectionString);
+            this.connection_retry_policy = connectionRetryPolicy;
+            this.command_retry_policy = commandRetryPolicy;
         }
 
+        /// <inheritdoc />
         /// <summary>
         /// Gets or sets the connection string for opening a connection to the SQL Database.
         /// </summary>
         public string ConnectionString
         {
-            get => connectionString;
+            get => connection_string;
             set
             {
-                connectionString = value;
-                underlyingConnection.ConnectionString = value;
+                connection_string = value;
+                underlying_connection.ConnectionString = value;
             }
         }
 
@@ -94,34 +103,34 @@ namespace roundhouse.databases.sqlserver
         /// Gets the policy that determines whether to retry a connection request, based on how many
         /// times the request has been made and the reason for the last failure.
         /// </summary>
-        public RetryPolicy ConnectionRetryPolicy => connectionRetryPolicy;
+        public RetryPolicy ConnectionRetryPolicy => connection_retry_policy;
 
         /// <summary>
         /// Gets the policy that determines whether to retry a command, based on how many
         /// times the request has been made and the reason for the last failure.
         /// </summary>
-        public RetryPolicy CommandRetryPolicy => commandRetryPolicy;
+        public RetryPolicy CommandRetryPolicy => command_retry_policy;
 
         /// <summary>
         /// Gets an instance of the SqlConnection object that represents the connection to a SQL Database instance.
         /// </summary>
-        public SqlConnection Current => underlyingConnection;
+        public SqlConnection Current => underlying_connection;
 
        
         /// <summary>
         /// Gets a value that specifies the time to wait while trying to establish a connection before terminating
         /// the attempt and generating an error.
         /// </summary>
-        public int ConnectionTimeout => underlyingConnection.ConnectionTimeout;
+        public int ConnectionTimeout => underlying_connection.ConnectionTimeout;
 
         /// <summary>
         /// Gets the name of the current database or the database to be used after a
         /// connection is opened.
         /// </summary>
-        public string Database => underlyingConnection.Database;
+        public string Database => underlying_connection.Database;
 
         /// <summary>Gets the current state of the connection.</summary>
-        public ConnectionState State => underlyingConnection.State;
+        public ConnectionState State => underlying_connection.State;
 
         /// <summary>
         /// Opens a database connection with the settings specified by the ConnectionString and ConnectionRetryPolicy properties.
@@ -141,11 +150,11 @@ namespace roundhouse.databases.sqlserver
         {
             retryPolicy.Execute(() =>
                 {
-                    if (underlyingConnection.State == ConnectionState.Open)
+                    if (underlying_connection.State == ConnectionState.Open)
                         return;
-                    underlyingConnection.Open();
+                    underlying_connection.Open();
                 });
-            return underlyingConnection;
+            return underlying_connection;
         }
 
         /// <summary>
@@ -190,72 +199,72 @@ namespace roundhouse.databases.sqlserver
         /// </summary>
         /// <typeparam name="T">IDataReader, XmlReader, or any other .NET Framework type that defines the type of result to be returned.</typeparam>
         /// <param name="command">The SQL command to be executed.</param>
-        /// <param name="retryPolicy">The retry policy that defines whether to retry a command if a connection fails while executing the command.</param>
+        /// <param name="retry_policy">The retry policy that defines whether to retry a command if a connection fails while executing the command.</param>
         /// <param name="behavior">A description of the results of the query and its effect on the database.</param>
         /// <returns>An instance of an IDataReader, XmlReader, or any other .NET Frameork object that contains the result.</returns>
-        public T ExecuteCommand<T>(IDbCommand command, RetryPolicy retryPolicy, CommandBehavior behavior)
+        public T ExecuteCommand<T>(IDbCommand command, RetryPolicy retry_policy, CommandBehavior behavior)
         {
-            T actionResult = default(T);
-            Type resultType = typeof(T);
-            bool hasOpenedConnection = false;
-            bool closeOpenedConnectionOnSuccess = false;
+            T action_result = default(T);
+            Type result_type = typeof(T);
+            bool has_opened_connection = false;
+            bool close_opened_connection_on_success = false;
             try
             {
-                retryPolicy.Execute((Action) (() => actionResult =
-                    connectionStringFailoverPolicy.Execute<T>( (() =>
+                retry_policy.Execute((Action) (() => action_result =
+                    connection_string_failover_policy.Execute<T>( (() =>
                     {
                         if (command.Connection == null)
                         {
                             command.Connection = Open();
-                            hasOpenedConnection = true;
+                            has_opened_connection = true;
                         }
 
                         if (command.Connection.State != ConnectionState.Open)
                         {
                             command.Connection.Open();
-                            hasOpenedConnection = true;
+                            has_opened_connection = true;
                         }
 
-                        if (typeof(IDataReader).IsAssignableFrom(resultType))
+                        if (typeof(IDataReader).IsAssignableFrom(result_type))
                         {
-                            closeOpenedConnectionOnSuccess = false;
+                            close_opened_connection_on_success = false;
                             return (T) command.ExecuteReader(behavior);
                         }
 
-                        if (resultType == typeof(XmlReader))
+                        if (result_type == typeof(XmlReader))
                         {
                             if (!(command is SqlCommand))
                                 throw new NotSupportedException();
-                            XmlReader innerReader = (command as SqlCommand).ExecuteXmlReader();
-                            closeOpenedConnectionOnSuccess = false;
+                            XmlReader inner_reader = (command as SqlCommand).ExecuteXmlReader();
+                            close_opened_connection_on_success = false;
                             if ((behavior & CommandBehavior.CloseConnection) != CommandBehavior.CloseConnection)
-                                return (T) (object) innerReader;
+                                return (T) (object) inner_reader;
                             else
                                 throw new ApplicationException("Don't have any SqlXmlReader");
                         }
 
-                        if (resultType == typeof(NonQueryResult))
+                        if (result_type == typeof(NonQueryResult))
                         {
                             NonQueryResult nonQueryResult =
                                 new NonQueryResult()
                                 {
                                     RecordsAffected = command.ExecuteNonQuery()
                                 };
-                            closeOpenedConnectionOnSuccess = true;
-                            return (T) Convert.ChangeType(nonQueryResult, resultType,
+                            close_opened_connection_on_success = true;
+                            return (T) Convert.ChangeType(nonQueryResult, result_type,
                                 CultureInfo.InvariantCulture);
                         }
 
                         object obj = command.ExecuteScalar();
-                        closeOpenedConnectionOnSuccess = true;
+                        close_opened_connection_on_success = true;
                         if (obj != null)
-                            return (T) Convert.ChangeType(obj, resultType,
+                            return (T) Convert.ChangeType(obj, result_type,
                                 CultureInfo.InvariantCulture);
                         return default(T);
                     }))));
-                if (hasOpenedConnection)
+                if (has_opened_connection)
                 {
-                    if (closeOpenedConnectionOnSuccess)
+                    if (close_opened_connection_on_success)
                     {
                         if (command.Connection != null)
                         {
@@ -267,13 +276,13 @@ namespace roundhouse.databases.sqlserver
             }
             catch (Exception)
             {
-                if (hasOpenedConnection && command.Connection != null &&
+                if (has_opened_connection && command.Connection != null &&
                     command.Connection.State == ConnectionState.Open)
                     command.Connection.Close();
                 throw;
             }
 
-            return actionResult;
+            return action_result;
         }
 
         /// <summary>
@@ -304,14 +313,14 @@ namespace roundhouse.databases.sqlserver
         /// <returns>An object that represents the new transaction.</returns>
         public IDbTransaction BeginTransaction(IsolationLevel il)
         {
-            return underlyingConnection.BeginTransaction(il);
+            return underlying_connection.BeginTransaction(il);
         }
 
         /// <summary>Begins a database transaction.</summary>
         /// <returns>An object that represents the new transaction.</returns>
         public IDbTransaction BeginTransaction()
         {
-            return underlyingConnection.BeginTransaction();
+            return underlying_connection.BeginTransaction();
         }
 
         /// <summary>
@@ -320,7 +329,7 @@ namespace roundhouse.databases.sqlserver
         /// <param name="databaseName">The name of the database to use in place of the current database.</param>
         public void ChangeDatabase(string databaseName)
         {
-            underlyingConnection.ChangeDatabase(databaseName);
+            underlying_connection.ChangeDatabase(databaseName);
         }
 
         void IDbConnection.Open()
@@ -331,7 +340,7 @@ namespace roundhouse.databases.sqlserver
         /// <summary>Closes the connection to the database.</summary>
         public void Close()
         {
-            underlyingConnection.Close();
+            underlying_connection.Close();
         }
 
         /// <summary>
@@ -340,12 +349,12 @@ namespace roundhouse.databases.sqlserver
         /// <returns>A System.Data.SqlClient.SqlCommand object that is associated with the underlying connection.</returns>
         public SqlCommand CreateCommand()
         {
-            return underlyingConnection.CreateCommand();
+            return underlying_connection.CreateCommand();
         }
 
         IDbCommand IDbConnection.CreateCommand()
         {
-            return underlyingConnection.CreateCommand();
+            return underlying_connection.CreateCommand();
         }
 
         object ICloneable.Clone()
@@ -373,9 +382,9 @@ namespace roundhouse.databases.sqlserver
         {
             if (!disposing)
                 return;
-            if (underlyingConnection.State == ConnectionState.Open)
-                underlyingConnection.Close();
-            underlyingConnection.Dispose();
+            if (underlying_connection.State == ConnectionState.Open)
+                underlying_connection.Close();
+            underlying_connection.Dispose();
         }
 
         /// <summary>
